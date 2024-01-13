2edaf73032cfb13d7c84940b56ef393edf88f11e
Kain <mtheusmss@hotmail.com>
2024-01-13 14:36:52 -0300

    feat: reaproveitamento layout html

---
+++ Includes/layout-cabecalho.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="../Assets/favicon.png" type="image/x-icon">
+    <title><?= $title ?></title>
+</head>
+<body style="font-family: 'Roboto Mono' monospace">
