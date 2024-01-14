5619d8b8fef56358d59d360e30a143f1fa098b57
Kain <mtheusmss@hotmail.com>
2024-01-14 16:17:53 -0300

    feat: criação de máscaras nos inputs

--- Includes/layout-rodape.php
+++ Includes/layout-rodape.php
@@ -1,11 +1,16 @@
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <script>
         var j = jQuery.noConflict();
         j(document).ready(function(){
+
+            j('#cpf').mask('000.000.000-00');
+            j('#data_criacao').mask('00/00/0000');
+            
             j('.carousel').owlCarousel({
                 margin:20,
                 loop: true,
