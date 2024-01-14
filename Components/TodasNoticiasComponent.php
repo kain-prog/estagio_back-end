39a019542156577849bf29a4eb10bb12235278b6
Kain <mtheusmss@hotmail.com>
2024-01-14 03:59:20 -0300

    feat: componente de todas as noticias

---
+++ Components/TodasNoticiasComponent.php
@@ -0,0 +1,14 @@
+<a href="#" class="card mb-3  text-decoration-none" style="max-width: 548px; transition: all ease 0.3s" onmouseover="this.style.scale = (1.02); this.style.background = '#e2e8ed';" onmouseout="this.style.scale = (1.0); this.style.background = 'none';" >
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="#" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
+</a>
