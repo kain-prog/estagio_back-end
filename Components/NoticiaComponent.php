4941dfdb9efe370d0dbec879317b80f226d57031
Kain <mtheusmss@hotmail.com>
2024-01-14 03:58:55 -0300

    feat: componente de noticia id

---
+++ Components/NoticiaComponent.php
@@ -0,0 +1,19 @@
+<div class="my-5">
+    <h1 class="text-center"> titulo </h1>
+    <p class="text-center"> resumo </p>
+</div>
+
+<div class="m-auto" style="max-width: 750px;">
+    <img src="" alt="">
+    <p class="w-100 text-end card-text"><small class="text-body-secondary"> data </small></p>
+</div>
+
+<div class="m-auto w-100" style="max-width: 750px;">
+    <p>conteudo</p>
+</div>
+
+<div class="container w-100 d-flex justify-content-end">
+    <form method="POST">
+        <input class="nav-link text-primary" type="submit" name="ver_noticias" value="Voltar para todas as notícias">
+    </form>
+</div>
