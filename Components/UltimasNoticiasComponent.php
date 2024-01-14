f34d43f3c0527993fc3f3347dc83bf91b8faf146
Kain <mtheusmss@hotmail.com>
2024-01-14 04:00:21 -0300

    feat: componente de ultimas noticias

---
+++ Components/UltimasNoticiasComponent.php
@@ -0,0 +1,68 @@
+<div class="carousel owl-carousel owl-loaded owl-drag ">
+    <div class="owl-stage-outer">
+        <div class="owl-stage d-flex" style="transform: translate3d(-4640px, 0px, 0px); transition: all 0.75s ease 0s; width: 8894px;">
+            
+            <div class="owl-item px-3 px-lg-0" style="max-width: 548px">
+                <a href="#" class="card mb-3  text-decoration-none" style="transition: all ease 0.3s" onmouseover="this.style.scale = (1.02); this.style.background = '#e2e8ed';" onmouseout="this.style.scale = (1.0); this.style.background = 'none';" >
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="#" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="owl-item px-3 px-lg-0" style="max-width: 548px;">
+                <a href="#" class="card mb-3 text-decoration-none" style="transition: all ease 0.3s" onmouseover="this.style.scale = (1.02); this.style.background = '#e2e8ed';" onmouseout="this.style.scale = (1.0); this.style.background = 'none';" >
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="#" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="owl-item px-3 px-lg-0" style="max-width: 548px;">
+                <a href="#" class="card mb-3 text-decoration-none" style="transition: all ease 0.3s" onmouseover="this.style.scale = (1.02); this.style.background = '#e2e8ed';" onmouseout="this.style.scale = (1.0); this.style.background = 'none';" >
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="#" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+    </div>
+    <div class="owl-nav disabled">
+        <div class="owl-prev">prev</div>
+        <div class="owl-next">next</div>
+    </div>
+    <div class="owl-dots">
+        <div class="owl-dot active"><span></span></div>
+        <div class="owl-dot"><span></span></div>
+        <div class="owl-dot"><span></span></div>
+    </div>
+
+</div>
