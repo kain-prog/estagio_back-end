7337d0a442d88ba81958b0d5bbd35a8f22e11260
Kain <mtheusmss@hotmail.com>
2024-01-14 03:11:32 -0300

    feat: inclusao jquery + owl carousel

--- Includes/layout-cabecalho.php
+++ Includes/layout-cabecalho.php
@@ -6,7 +6,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
     <link rel="shortcut icon" href="../Assets/favicon.png" type="image/x-icon">
     <title><?= $title ?></title>
 </head>
-<body style="font-family: 'Roboto Mono' monospace">
--- Includes/layout-rodape.php
+++ Includes/layout-rodape.php
@@ -1,4 +1,33 @@
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" ></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" ></script>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script>
+        var j = jQuery.noConflict();
+        j(document).ready(function(){
+            j('.carousel').owlCarousel({
+                margin:20,
+                loop: true,
+                autoplayTimeOut: 2000,
+                autoplayHoverPause: true,
+                responsive: {
+                    0:{
+                        items: 1,
+                        nav: false,
+                    },
+                    600:{
+                        items: 2,
+                        nav: false
+                    },
+                    1000:{
+                        items: 3,
+                        nav: false
+                    }
+                }
+            });
+        });
+
+    </script>
 </body>
 </html>
