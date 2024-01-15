863fd2f6d9cbb285d35fb4f91cdbbd8f9a0a953d
Kain <mtheusmss@hotmail.com>
2024-01-15 05:01:19 -0300

    feat: deixando o componente de todas as noticias dinamico

--- Components/TodasNoticiasComponent.php
+++ Components/TodasNoticiasComponent.php
@@ -1,14 +1,37 @@
-<a href="#" class="card mb-3  text-decoration-none" style="max-width: 548px; transition: all ease 0.3s" onmouseover="this.style.scale = (1.02); this.style.background = '#e2e8ed';" onmouseout="this.style.scale = (1.0); this.style.background = 'none';" >
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img src="#" class="img-fluid rounded-start" alt="...">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+<style>
+    .todas-noticias{
+        width: 560px;
+        height: 170px;
+        min-height: 170px;
+    }
+    .imagem-noticias{
+        width: 100%;
+    }
+    @media(max-width: 991px){
+        .todas-noticias{
+            height: 100%;
+            width: 100%;
+        }
+    }
+</style>
+
+<div class="row gap-4 container m-auto">
+    <?php foreach( $noticias as $noticia ): ?>
+
+    <a href="../noticias/info.php?id=<?= $noticia['id'] ?>" class="card txt-decoration-none todas-noticias text-decoration-none" style="transition: all ease 0.3s" onmouseover="this.style.scale = (1.02); this.style.background = '#e2e8ed';" onmouseout="this.style.scale = (1.0); this.style.background = 'none';" >
+        <div class="row h-100">
+            <div class="col-md-4 h-100">
+                <img src="../<?= $noticia['imagem'] ?>" class="h-100 rounded-start text-center imagem-noticias" alt="<?= $noticia['titulo'] ?>" style="">
+            </div>
+            <div class="col-md-8 h-100">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $noticia['titulo'] ?></h5>
+                    <p class="card-text"><?= $noticia['resumo'] ?></p>
+                    <p class="card-text"><small class="text-body-secondary"> <?= $noticia['data_criacao'] ?> </small></p>
+                </div>
             </div>
         </div>
-    </div>
-</a>
+    </a>
+
+    <?php endforeach ?>
+</div>
