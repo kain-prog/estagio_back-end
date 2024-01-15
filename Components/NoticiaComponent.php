1a8e109fd437d13db5ef0a57f0a23135fd685e76
Kain <mtheusmss@hotmail.com>
2024-01-15 05:18:52 -0300

    feat: setando o componente de informação da noticia dinamico

--- Components/NoticiaComponent.php
+++ Components/NoticiaComponent.php
@@ -1,19 +1,17 @@
 <div class="my-5">
-    <h1 class="text-center"> titulo </h1>
-    <p class="text-center"> resumo </p>
+    <h1 class="text-center"> <?= $noticia['titulo'] ?> </h1>
+    <p class="text-center"> <?= $noticia['resumo'] ?> </p>
 </div>
 
-<div class="m-auto" style="max-width: 750px;">
-    <img src="" alt="">
-    <p class="w-100 text-end card-text"><small class="text-body-secondary"> data </small></p>
+<div class="m-auto d-flex flex-column mb-3" style="max-width: 750px;">
+    <img class="my-3 m-auto" src="../<?= $noticia['imagem'] ?>" alt="" style="max-width: 450px;" >
+    <p class="w-100 text-end card-text"><small class="text-body-secondary"> <?= $noticia['data_criacao'] ?> </small></p>
 </div>
 
 <div class="m-auto w-100" style="max-width: 750px;">
-    <p>conteudo</p>
+    <p><?= $noticia['conteudo'] ?></p>
 </div>
 
 <div class="container w-100 d-flex justify-content-end">
-    <form method="POST">
-        <input class="nav-link text-primary" type="submit" name="ver_noticias" value="Voltar para todas as notícias">
-    </form>
+    <a class="text-decoration-none" href="../noticias/todas.php">Voltar para todas as notícias</a>
 </div>
