ea2a37224f7c112bc4603241be94eaca0eb121a4
Kain <mtheusmss@hotmail.com>
2024-01-15 22:40:49 -0300

    feat: criação dos botoes destacar e remover destacado para admin

--- Components/TodasNoticiasComponent.php
+++ Components/TodasNoticiasComponent.php
@@ -24,14 +24,36 @@
                 <img src="../<?= $noticia['imagem'] ?>" class="h-100 rounded-start text-center imagem-noticias" alt="<?= $noticia['titulo'] ?>" style="object-fit: cover; object-position: left;">
             </div>
             <div class="col-md-8 h-100">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $noticia['titulo'] ?></h5>
+                <div class="card-body px-0 h-100">
+
+                    <div class="d-flex justify-content-between">
+                        <h5 class="card-title"><?= $noticia['titulo'] ?></h5>
+
+                        <form method="POST">
+                            <input type="hidden" name="id" value="<?= $noticia['id'] ?>">
+                            
+                            <?php if( $usuario_logado['adm'] ): ?>
+
+                                <?php if( $noticia['destaque'] ){ ?>
+
+                                    <input class="btn btn-sm text-white" name="destacar" type="submit" value="Remover Destaque" style="font-size: 10px; background: #315d7b">
+
+                                <?php }else { ?>
+
+                                    <input class="btn btn-primary btn-sm" name="destacar" type="submit" value="Destacar" style="font-size: 10px;">
+
+                                <?php } ?>
+
+                            <?php endif ?>
+
+                        </form>
+
+                    </div>
+
                     <p class="card-text"><?= $noticia['resumo'] ?></p>
                     <p class="card-text"><small class="text-body-secondary"> <?= $noticia['data_criacao'] ?> </small></p>
 
-                </div>
-
-                <div class="d-flex py-3 py-lg-0 justify-content-end gap-2">
+                    <div class="d-flex py-3 py-lg-0 justify-content-end gap-2">
                     <?php if( $usuario_logado['adm'] ): ?>
                         <form method="GET">
                             <input type="hidden" name="id" value="<?= $noticia['id'] ?>">
@@ -43,6 +65,10 @@
                         </form>
                     <?php endif ?>
                 </div>
+
+                </div>
+
+                
 
             </div>
         </div>
