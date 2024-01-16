84fbb1c2b666657029c07a2c32d7d0d35dc72d09
Kain <mtheusmss@hotmail.com>
2024-01-16 17:53:11 -0300

    feat: página de informação do produto

---
+++ produtos/info.php
@@ -0,0 +1,90 @@
+<?php
+
+    session_start();
+
+    require '../Database/Conexao.php';
+    require '../Class/UsuariosClass.php';
+    require '../Class/ProdutosClass.php';
+    
+    if( empty( $_GET['id']) ){
+        echo "<script>alert( 'Não foi possível acessar esta página' )</script>";
+        header( 'Location: ../login.php' );
+        exit;
+    }
+
+    if( !empty( $_SESSION['internit-login'] ) ){
+
+        $id = $_SESSION['internit-login'];
+        $usuarios_class = new Usuarios( $pdo );
+        $usuario_logado = $usuarios_class->listar_por_id( $id );
+
+        $id_produto = $_GET['id'];
+        $produtos_class = new Produtos( $pdo );
+        $produto = $produtos_class->listar_produto_por_id( $id_produto )->fetch();
+
+        if( $produto['usuario_id'] != $usuario_logado['id'] ){
+            echo "<script>alert( 'Não foi possível acessar esta página' )</script>";
+            header( 'Location: ../login.php' );
+            exit;
+        }
+
+        if( !empty( $_POST['id'] && $_POST['ativar'] )){
+
+            $ativar = $produtos_class->ativar_toggle( $id_produto );
+
+            if( $ativar['sucesso'] ) {
+                header( 'Refresh: 0' );
+                exit;
+            };
+        }
+
+        if( !empty( $_POST['id'] && $_POST['ativar'] )){
+
+            $ativar = $produtos_class->ativar_toggle( $id_produto );
+
+            if( $ativar['sucesso'] ) {
+                header( 'Refresh: 0' );
+                exit;
+            };
+        }
+        
+        if( !empty( $_POST['excluir_produto'] )){
+
+            $ativar = $produtos_class->excluir_produto( $id_produto );
+
+            if( $ativar['sucesso'] ) {
+                header( 'Refresh: 0' );
+                exit;
+            };
+        }
+
+
+    }else{
+        header( 'Location: ../login.php' );
+        exit;
+    }
+
+    if( !empty( $_POST['logout'] )){
+        $usuarios = new Usuarios( $pdo );
+        $usuarios->logout();
+    
+        header( 'Location: ../index.php' );
+        exit;        
+    }
+
+    $title = "Produto - " . $produto['nome'];
+
+    include '../Includes/layout-cabecalho.php';
+?>
+
+<?php require '../Components/MenuComponent.php' ?>
+
+
+<div class="container m-auto mt-5 pt-5">
+
+    <?php require '../Components/ProdutoComponent.php' ?>
+
+</div>
+
+
+<?php include '../Includes/layout-rodape.php' ?>
