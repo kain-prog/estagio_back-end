8c9c4fb95ad8317e914910072ca452cea8b6ba1a
Kain <mtheusmss@hotmail.com>
2024-01-16 04:12:53 -0300

    style: setando estilo pro carousel de ultimas noticias [noticias em destaque]

--- Components/UltimasNoticiasComponent.php
+++ Components/UltimasNoticiasComponent.php
@@ -1,3 +1,10 @@
+<style>
+    .image-thumb{
+        object-fit: cover;
+        object-position: left;
+    }
+</style>
+
 <div class="carousel owl-carousel owl-loaded owl-drag ">
     <div class="owl-stage-outer">
         <div class="owl-stage d-flex" style="transform: translate3d(-4640px, 0px, 0px); transition: all 0.75s ease 0s; width: 8894px;">
@@ -6,12 +13,12 @@
 
                 <div class="owl-item px-3 px-lg-0" style="max-width: 548px; min-height: 160px;">
                     <a href="../noticias/info.php?id=<?= $noticia['id'] ?>" class="card h-100 text-decoration-none" style="transition: all ease 0.3s" onmouseover="this.style.scale = (1.02); this.style.background = '#e2e8ed';" onmouseout="this.style.scale = (1.0); this.style.background = 'none';" >
-                        <div class="row g-0">
-                            <div class="col-md-4">
-                                <img src="../<?= $noticia['imagem'] ?>" class="rounded-start text-center w-100" alt="<?= $noticia['titulo'] ?>" style="">
+                        <div class="row g-0 h-100">
+                            <div class="col-md-4 h-100">
+                                <img src="../<?= $noticia['imagem'] ?>" class="image-thumb h-100 rounded-start text-center w-100" alt="<?= $noticia['titulo'] ?>" style="">
                             </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
+                            <div class="col-md-8 h-100">
+                                <div class="card-body h-100">
                                     <h5 class="card-title"><?= $noticia['titulo'] ?></h5>
                                     <p class="card-text"><?= $noticia['resumo'] ?></p>
                                     <p class="card-text"><small class="text-body-secondary"> <?= $noticia['data_criacao'] ?> </small></p>
