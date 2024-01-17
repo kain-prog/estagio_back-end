88101d98d4299688456079c2c9a71bf4ad296f8f
Kain <mtheusmss@hotmail.com>
2024-01-17 04:40:38 -0300

    feat: inserção de notícias no painel usuário

--- usuario/painel.php
+++ usuario/painel.php
@@ -5,6 +5,7 @@
 
     require '../Database/Conexao.php';
     require '../Class/UsuariosClass.php';
+    require '../Class/NoticiasClass.php';
     
     include '../Includes/layout-cabecalho.php';
 
@@ -12,8 +13,15 @@
 
         $id = $_SESSION['internit-login'];
 
-        $usuarios = new Usuarios( $pdo );
-        $usuario_logado = $usuarios->listar_por_id( $id );
+        $usuarios_class = new Usuarios( $pdo );
+        $usuario_logado = $usuarios_class->listar_por_id( $id );
+
+        $noticias_class = new Noticias( $pdo );
+        // Todas Noticias
+        $quantidade_noticias = $noticias_class->todas_noticias()->rowCount();
+        // Noticias em Destaque
+        $quantidade_noticias_destaque = $noticias_class->noticias_em_destaque()->rowCount();;
+        $noticias_destaque = $noticias_class->noticias_em_destaque()->fetchAll();
 
         if( $usuario_logado['adm'] ){
             header( 'Location: ../adm/painel.php' );
@@ -32,6 +40,11 @@
         header( 'Location: ../index.php' );
         exit;        
     }
+
+    if( !empty( $_POST['ver_noticias'] )){
+        header( 'Location: ../noticias/todas.php' );
+        exit;      
+    }
 ?>
 
 <?php require '../Components/MenuComponent.php' ?>
@@ -39,25 +52,33 @@
 <div class="container pt-5 m-auto">
 
 <div class="mb-5 row justify-content-center justify-content-lg-start">
-        <div class="col-lg-4 mb-3 mb-md-0 mt-auto">
-            <h2 class="">Painel de Assinantes</h2>
-            <p class="">Olá, <?= $usuario_logado['nome'] ?>.  Bem vindo novamente.</p>
-           
-            <div class="mt-3 d-flex justify-content-between">
-                <a href="#">Editar perfil</a>
-           
-            </div>
+        <div class="col-lg-4 mb-3 mb-md-0 flex-column w-auto">
+            <h2>Painel de Assinantes</h2>
+            <p>Olá, <?= $usuario_logado['nome'] ?>.  Bem vindo novamente.</p>
         </div>
 
-
-        <div class="col-lg-8 d-flex flex-column gap-2 flex-wrap align-items-center align-items-lg-end">
+        <div class="col-lg-8 d-flex flex-wrap justify-content-center align-items-center justify-content-lg-end">
                 
-            <p class="mb-1">Email: <?= $usuario_logado['email'] ?></p>
-            <div class="d-flex gap-4 mb-1">
-                <p>Cidade: <?= $usuario_logado['cidade'] ?></p>
-                <p>UF: <?= $usuario_logado['uf'] ?></p>
+            <div class="w-auto">
+                <div class="mt-3 w-100 d-flex justify-content-between align-items-center">
+
+                    <h5 class="text-secondary opacity-75"><?= $usuario_logado['nome'] ?></h5>
+
+                    <small><a class="text-decoration-none" href="./perfil.php">Editar perfil</a></small>
+
+                </div>
+
+                <p class="mb-1 text-secondary opacity-75">Email: <?= $usuario_logado['email'] ?></p>
+               
+                <div class="d-flex mb-1" style="gap: 3.8rem">
+                    <p class="mb-0 text-secondary opacity-75">Cidade: <?= $usuario_logado['cidade'] ?></p>
+                    <p class="mb-0 text-secondary opacity-75">UF: <?= $usuario_logado['uf'] ?></p>
+                </div>
+
+                <p class="mb-5 text-secondary opacity-75">CPF: <?= $usuario_logado['cpf'] ?></p>
+
+                <a class="btn text-white w-100" href="../dashboard.php" style="background: #315d7b">Ir para Dashboard</a>
             </div>
-            <p class="mb-1">CPF: <?= $usuario_logado['cpf'] ?></p>
 
         </div>
 
@@ -65,15 +86,21 @@
 
     <hr class="my-5" >
 
-    <div class="row m-auto">
+</div>
 
-        <?php require('../Components/NoticiasComponent.php')  ?>
+<div class="container m-auto">
+    <h6 class="mb-3">Confira as últimas notícias: </h6>
+</div>
 
-    </div>
+<?php require '../Components/UltimasNoticiasComponent.php' ?>
 
+<div class="container w-100 d-flex justify-content-end">
+    <form method="POST">
+        <input class="nav-link text-primary" type="submit" name="ver_noticias" value="Veja mais notícias">
+    </form>
 </div>
 
 
 <?php 
-    include './Includes/layout-rodape.php'; 
+    include '../Includes/layout-rodape.php'; 
 ?>
