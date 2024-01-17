04204658ddb93542bdd9226d333ebc657400159d
Kain <mtheusmss@hotmail.com>
2024-01-17 15:10:47 -0300

    feat: pagina de editar categoria funcional

--- categorias/editar.php
+++ categorias/editar.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    $title = "Editar Produto";
+    $title = "Editar Categoria";
 
     require '../Database/Conexao.php';
     require '../Class/UsuariosClass.php';
@@ -11,11 +11,6 @@
 
     if( !empty( $_SESSION['internit-login']) ){
 
-        if( $usuario_logado['adm'] ){
-            header( 'Location: ../usuario/painel.php' ); 
-            exit;
-        }
-
         //id do usuário logado
         $id = $_SESSION['internit-login'];
 
@@ -23,6 +18,11 @@
 
         // Dados do usuário logado
         $usuario_logado = $usuarios_class->listar_por_id( $id );
+
+        if( $usuario_logado['adm'] ){
+            header( 'Location: ../adm/painel.php' ); 
+            exit;
+        }
 
         if( !empty( $_GET['id'] )){
 
@@ -39,13 +39,12 @@
         
         }
 
-        if( !empty( $_POST['atualizar_categoria'] )){
+        if( !empty( $_POST['editar_categoria'] )){
 
             $categoria_id = $_GET['id'];
             $nome_categoria = $_POST['nome_categoria'] ;
-            $codigo_categoria = $_POST['codigo_categoria'];
           
-            $dados = array( 'nome_categoria' => $nome_categoria, 'codigo_categoria' => $codigo_categoria, 'usuario_id' => $usuario_logado['id'] );
+            $dados = array( 'nome_categoria' => $nome_categoria, 'usuario_id' => $usuario_logado['id'] );
         
             $resultado = $categorias_class->editar_categoria($categoria_id, $dados );
 
@@ -75,7 +74,7 @@
 
 <div class="container m-auto">
 
-    <h1 class="my-3 pb-3">Criar Categoria</h1>
+    <h1 class="my-3 pb-3">Editar Categoria</h1>
 
     <?php require '../Components/FormEditarCategoriaComponent.php' ?>
 
