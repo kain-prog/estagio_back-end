65caea43d686ee8aecd098b98bd006782c27d5da
Kain <mtheusmss@hotmail.com>
2024-01-17 00:43:46 -0300

    style: configurando estilo do carousel

--- Components/UltimasNoticiasComponent.php
+++ Components/UltimasNoticiasComponent.php
@@ -12,7 +12,7 @@
             <?php foreach( $noticias_destaque as $noticia ): ?>
 
                 <div class="owl-item px-3 px-lg-0" style="max-width: 548px; min-height: 160px;">
-                    <a href="../noticias/info.php?id=<?= $noticia['id'] ?>" class="card h-md-100 text-decoration-none" style="transition: all ease 0.3s" onmouseover="this.style.scale = (1.02); this.style.background = '#e2e8ed';" onmouseout="this.style.scale = (1.0); this.style.background = 'none';" >
+                    <a href="../noticias/info.php?id=<?= $noticia['id'] ?>" class="card h-100 text-decoration-none" style="min-height: 215px; transition: all ease 0.3s" onmouseover="this.style.scale = (1.02); this.style.background = '#e2e8ed';" onmouseout="this.style.scale = (1.0); this.style.background = 'none';" >
                         <div class="row g-0 h-100">
                             <div class="col-md-4">
                                 <img src="../<?= $noticia['imagem'] ?>" class="image-thumb h-100 rounded-start text-center w-100" alt="<?= $noticia['titulo'] ?>" style="">
