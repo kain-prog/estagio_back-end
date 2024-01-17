cff6fb951a44a2fd1229c492b3aef514178d0633
Kain <mtheusmss@hotmail.com>
2024-01-17 00:45:12 -0300

    feat: estilizando componente de informação do produto

--- Components/ProdutoComponent.php
+++ Components/ProdutoComponent.php
@@ -7,24 +7,24 @@
 
 <div class="card my-3 m-auto" style="max-width: 780px;">
   <div class="row g-0">
-    <div class="col-md-5">
-      <img src="../<?= $produto['imagem'] ?>" class="h-100 img-fluid rounded-start object-fit-cover" alt="<?= $produto['nome'] ?>">
+    <div class="col-md-12 w-100">
+      <img src="../<?= $produto['imagem'] ?>" class="w-100 img-fluid rounded-start object-fit-cover" alt="<?= $produto['nome'] ?>">
     </div>
-    <div class="col-md-7">
+    <div class="col-md-12">
       <div class="card-body w-100 pe-0 pt-0">
-        <div class="row w-100 pe-0 m-0">
+        <div class="row w-100 pe-3 m-0">
             <h5 class="card-title col-8 pt-3 ps-0 pe-3"><?= $produto['nome'] ?></h5>
-            <form class="col-4 p-0 pt-3 pt-lg-0 d-flex align-items-start justify-content-end" method="POST">
+            <form class="col-4 p-0 pt-3 d-flex align-items-start justify-content-end" method="POST">
                 
                 <?php if( $produto['situacao'] != 1 ){ ?>
 
                     <input type="hidden" name="id" value="<?= $produto['id'] ?>">
-                    <input class="btn btn-sm text-white" type="submit" name="ativar" value="Ativar Produto" style="background: #315d7b">
+                    <input class="btn btn-sm btn-success" type="submit" name="ativar" value="Ativar Produto">
 
                 <?php } else { ?>
 
                     <input type="hidden" name="id" value="<?= $produto['id'] ?>">
-                    <input class="btn btn-sm text-white" type="submit" name="ativar" value="Desativar Produto" style="background: #315d7b">
+                    <input class="btn btn-sm btn-danger" type="submit" name="ativar" value="Desativar Produto">
 
                 <?php } ?>
 
