e5f14cacbbfa277f9676fceee08549069bb20414
Kain <mtheusmss@hotmail.com>
2024-01-16 22:27:54 -0300

    feat: importacao jquerymaskmoney para input de valor

--- Includes/layout-rodape.php
+++ Includes/layout-rodape.php
@@ -1,5 +1,6 @@
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-maskmoney/3.0.2/jquery.maskMoney.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
@@ -9,7 +10,9 @@
         j(document).ready(function(){
 
             j('#cpf').mask('000.000.000-00');
-            j('#data_criacao').mask('00/00/0000');
+            j('#data_criacao').mask('00/00/0000');  
+            j('#quantidade').mask('000');  
+            j('#valor').maskMoney();   
             
             j('.carousel').owlCarousel({
                 margin:20,
